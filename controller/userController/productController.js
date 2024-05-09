e4cea834d5811d6dc7a6b62258dfd25c86f39fa5
ASHWIN <meashwinclarence@gmail.com>
2024-05-09 23:33:29 +0530

    Similar products added

--- controller/userController/productController.js
+++ controller/userController/productController.js
@@ -10,12 +10,17 @@ const productView=async (req,res)=>{
     // product details of the selected product from product collection
     const product=await productSchema.findById(productID)
 
+    // find the product within same category
+    const similarProducts=await productSchema.find({productCategory:product.productCategory,_id:{$ne:productID}})
+
+
+
     if(product.length===0){
         req.flash("errorMessage",'Product is currently unavailable')
         return res.redirect('/user/home')
     }
 
-    res.render('user/productDetail',{title:product.productBrand,product,alertMessage:req.flash('errorMessage')})
+    res.render('user/productDetail',{title:product.productBrand,product,similarProducts,alertMessage:req.flash('errorMessage')})
 
 
 
