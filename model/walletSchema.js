8d1b6c5903301d7ff64db17063a14d28f78960b1
ASHWIN <meashwinclarence@gmail.com>
2024-06-13 14:52:16 +0530

    fea: wallet added

---
+++ model/walletSchema.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose')
+
+const schema=new mongoose.Schema({
+    userID:{
+        type:String
+    },
+    balance:{
+        type:Number,
+        default:0
+    },
+    orderID:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"order"
+    }
+},{timestamps:true})
+
+module.exports=mongoose.model('wallet',schema)
